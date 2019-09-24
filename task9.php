9aa0b7c641c68dbb03cdb88df80fa6fd72d6eeec
susan <susan.shr10@gmail.com>
2019-09-24 15:47:37 +0545

    normal site first commit

---
+++ task9.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title> Website Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+    /* .fakeimg {
+      height: 200px;
+      background: #aaa;
+    } */
+  </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+      <a class="navbar-brand" href="#">
+        <img src="static/images/logo.jpg" width="50" height="50" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>   
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary" href="#">Login</a>
+          </li>
+        </ul>
+      </div>  
+    </nav>
+
+  <div class="container-fluid" style="margin-top:80px; padding-top: 10px">
+    <div class="row">
+      <div class="col-sm-8 bg-secondary text-white">
+        <h2>ABOUT</h2>
+        <p>
+          Sochware, native engineers working for Indigenous problems. 
+          We create hardware solutions, for locally existing glitches, and help develop Technical Nepal. 
+          SochWare is a clay-pot by techies for innovation of explored solution to support the traditional market with the arms of hardware. 
+          Initially we're supporting the agriculture with the help of Internet of things (iOT)
+          What if I could do better with the technology? Let's agriculture in a smart way.
+          Let's do it like a boss. Let SochWare work for you. 
+        </p>        
+      </div>
+    
+      <div class="col-sm-4">
+        <div>
+          <img src="static/images/sochware.jpg" width="400" height="180" alt="Sochware Image">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid" style="margin-top: 40px">
+    <div class="row">
+      <div class="col-sm-4">
+        <h3>What we work on</h3>
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action">Websites</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-success">Mobile Apps</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">Data Warehousing</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-info">Web Hosting</a>
+          <a href="#" class="list-group-item list-group-item-action list-group-item-warning">IOT</a>
+        </div>
+        <hr class="d-sm-none">
+      </div>
+      <div class="col-sm-8 bg-dark text-white">
+        <div class="row">
+          <div class="col-sm-3">
+            <h3>Sochware1</h3>
+            <a href="#">
+            <img src="static/images/1.jpg" alt="Sochware1" width="100" height="100">
+            </a>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <h3>Sochware2</h3>
+            <a href="#">
+            <img src="static/images/2.jpg" alt="Sochware2" width="100" height="100">
+            </a>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <h3>Sochware3</h3>
+            <a href="#">
+            <img src="static/images/3.jpg" alt="Sochware3" width="100" height="100">
+            </a>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <h3>Sochware4</h3>
+            <a href="#">
+            <img src="static/images/4.jpg" alt="Sochware4" width="100" height="100">
+            </a>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bg-secondary" style="margin-top: 5px">
+    <marquee behavior="" direction="">
+    <div class="row">
+      <div class="col-sm-4 text-right">
+        <strong>Our Sponsers</strong>
+      </div>
+      <div class="col-sm-2">
+        <img src="static/images/op.jpg" alt="One Piece Character/s" width="50" height="50" class="mx-auto d-block">
+      </div>
+      <div class="col-sm-2">
+        <img src="static/images/op1.jpg" alt="One Piece Character/s" width="50" height="50" class="mx-auto d-block">
+      </div>
+      <div class="col-sm-2">
+        <img src="static/images/op2.jpg" alt="One Piece Character/s" width="50" height="50" class="mx-auto d-block">
+      </div>
+      <div class="col-sm-2">
+        <img src="static/images/op3.jpg" alt="One Piece Character/s" width="50" height="50" class="mx-auto d-block">
+      </div>
+    </div>
+  </marquee>
+  </div>
+  <div class="jumbotron text-center" style="margin-bottom:0">
+    <div class="row">
+      <div class="col-sm-2">
+        <a href="#">
+          <i class="fas fa-briefcase"></i> Jobs
+        </a>
+      </div>
+      <div class="col-sm-2">
+        <a href="#">
+          <i class="fas fa-blog"></i> Blogs
+        </a>
+      </div>
+      <div class="col-sm-2">
+        <a href="#">
+          <i class="fab fa-linkedin"></i> Linked In
+        </a>
+      </div>
+      <div class="col-sm-2">
+        <a href="#">
+          <i class="fab fa-twitter"></i> Twitter
+        </a>
+      </div>
+      <div class="col-sm-2">
+        <a href="#">
+          <i class="fab fa-instagram"></i> Instagram
+        </a>
+      </div>
+      <div class="col-sm-2">
+        <a href="#">
+          <i class="fab fa-facebook-f"></i> Facebook
+        </a>
+      </div>
+    </div>
+    <br>
+    <div class="mx-auto d-block">
+      <h3>Contact us</h3>
+      <div class="row">
+        <div class="col-sm-4">
+          <i class="fas fa-map-marker-alt"></i> <strong> 44 Shirley Ave. West Chicago,IL 60185</strong>
+        </div>
+        <div class="col-sm-4">
+          <i class="fa fa-phone"></i> <strong> 123.456.7890</strong>
+        </div>
+        <div class="col-sm-4">
+          <i class="far fa-envelope"></i> <strong> sochware@gmail.com</strong>
+        </div>
+      </div>
+    </div>
+    <br>
+    <h4>Copy right: &copy;Sochware</h4>
+  </div>
+
+</body>
+</html>
