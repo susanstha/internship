e2b118d2f32832cb51e73fe9d749706eb9e72702
susan <susan.shr10@gmail.com>
2019-11-20 11:27:20 +0545

    minute changes

--- PHP/fileup.php
+++ PHP/fileup.php
@@ -21,37 +21,37 @@
                 if(isset($_POST["submit"])) {
                     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
                     if($check !== false) {
-                        echo "File is an image - " . $check["mime"] . ".";
+                        echo "File is an image - " . $check["mime"] . ". <br>";
                         $uploadOk = 1;
                     } else {
-                        echo "File is not an image.";
+                        echo "File is not an image.<br>";
                         $uploadOk = 0;
                     }
                 }
                 // Check if file already exists
                 if (file_exists($target_file)) {
-                    echo "Sorry, file already exists.";
+                    echo "Sorry, file already exists.<br>";
                     $uploadOk = 0;
                 }
                 // Check file size
                 if ($_FILES["fileToUpload"]["size"] > 200000) {
-                    echo "Sorry, your file is too large.";
+                    echo "Sorry, your file is too large.<br>";
                     $uploadOk = 0;
                 }
                 // Allow certain file formats
                 if($imageFileType != "jpg" && $imageFileType != "png" ) {
-                    echo "Sorry, only JPG & PNG files are allowed.";
+                    echo "Sorry, only JPG & PNG files are allowed.<br>";
                     $uploadOk = 0;
                 }
                 // Check if $uploadOk is set to 0 by an error
                 if ($uploadOk == 0) {
-                    echo "Sorry, your file was not uploaded.";
+                    echo "Sorry, your file was not uploaded.<br>";
                 // if everything is ok, try to upload file
                 } else {
                     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
                     } else {
-                        echo "Sorry, there was an error uploading your file.";
+                        echo "Sorry, there was an error uploading your file.<br>";
                     }
                 }
             ?>
